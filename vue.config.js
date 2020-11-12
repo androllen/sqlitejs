e4d7d6079f6abd9b1ae8966c426fcf55a20f3f43
androllen <androllen@hotmail.com>
2020-11-12 18:10:33 +0800

    auto build package

--- vue.config.js
+++ vue.config.js
@@ -2,17 +2,55 @@ module.exports = {
   pluginOptions: {
     electronBuilder: {
       builderOptions: {
+        "appId": "com.sqlitejs3.app",
+        // app.exe name
+        "productName": "SqliteJs3", 
+        "copyright": "Copyright © 2020",
+        "directories": {
+          "output": "./dist_electron"
+        },
         // options placed here will be merged with default configuration and passed to electron-builder
-        // files: [
-        //   "**/*"
-        // ],
-        extraFiles: [
+        "files": [
+          "**/*",
+        ],
+        "extraFiles": [
           {
             "from": "TXThrift",
-            "to": "TXThrift",
+            "to": "./resources/TXThrift",
             "filter": ["**/*"]
           }
-        ]
+        ],
+        "nsis": {
+          "oneClick": false,
+          "perMachine": true,
+          "guid": "7e51495b-3f4d-5235-aadd-5636863064f1",
+          "allowElevation": true,
+          "allowToChangeInstallationDirectory": true,
+          //installer ico
+          "installerIcon": "./public/nsis/installer.ico",
+          //uninstaller ico
+          "uninstallerIcon": "./public/nsis/uninstaller.ico",
+          "installerHeaderIcon": "./public/nsis/installer.ico",
+          "createDesktopShortcut": true,
+          "createStartMenuShortcut": true,
+          //shortcut name
+          "shortcutName": "SqliteJSp",
+          "license": "./public/nsis/LICENSE",
+        },
+        "win": {
+          // app ico
+          "icon": "./public/app.ico",
+          "target": [
+            {
+              "target": "nsis",
+              "arch": [
+                "x64",
+                "ia32"
+              ]
+            }
+          ]
+        },
+        "asar": false
       },
       // List native deps here if they don't work
       externals: ['sqlite3'],
